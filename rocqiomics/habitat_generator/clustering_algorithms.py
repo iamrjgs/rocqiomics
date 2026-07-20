0017bb38499af815f65cc9447dde60aee43311f7
Garcia Santiago <ri54995@RoswellPark.org>
2026-07-20 13:54:52 -0400

    adds support for fuzzy c means

--- rocqiomics/habitat_generator/clustering_algorithms.py
+++ rocqiomics/habitat_generator/clustering_algorithms.py
@@ -67,15 +67,75 @@ class FuzzyCMeansClustering(VoxelClusteringAlgorithm):
 
     def _init_model(self):
         try:
-            from fuzzy_c_means import FuzzyCMeans
-            self.model = FuzzyCMeans(
-                n_clusters=self.n_clusters,
-                **self.kwargs
-            )
+            import skfuzzy
+
+            """
+            Scikit-fuzzy implements Fuzzy C Means algorithm as fit and predict functions
+            (skfuzzy.cmeans and skfuzzy.cmeans_predicts), not as a class.
+            """
+            self.model = None 
+
+            self.centroids = None
         except ImportError:
             raise ImportError(
-                "FuzzyCMeans clustering requires the scikit-fuzzy-c-means package. "
+                "FuzzyCMeans clustering requires the scikit-fuzzy package. "
                 "Please install this package or choose another algorithm. "
                 )
         except Exception as e:
             raise(e)
+        
+    def get_labels_from_probs(self, u):
+        return np.argmax(u, axis=0)
+    
+    def _apply_normalization_and_weights(self, X):
+        if self.mean_ is not None and self.std_ is not None:
+            n_int = len(self.mean_)
+            X_int = (X[:, :n_int] - self.mean_) / self.std_
+            
+            if X.shape[1] > n_int:
+                # If there are more channels than means, only normalize the first n_int channels
+                # This will happen, e.g. when spatial features are included, since mean/std are
+                # not computed for these.
+                X = np.concatenate([X_int, X[:, n_int:]], axis=1)
+            else:
+                X = X_int
+
+        if self.weights is not None:
+            if X.shape[1] != len(self.weights):
+                raise ValueError(
+                    f"Feature dimension mismatch: X has {X.shape[1]} features, "
+                    f"but weights has length {len(self.weights)}"
+                )
+            X = X * self.weights
+
+        return X
+    
+    def fit(self, X):
+        X = self._apply_normalization_and_weights(X)
+        cntr, u, u0, d, jm, p, fpc = skfuzzy.cmeans(
+            data=X.T, 
+            c=self.n_clusters,
+            **self.kwargs
+        )
+        self.centroids = cntr
+        return cntr, u, u0, d, jm, p, fpc
+
+    def fit_predict(self, X):
+        cntr, u, u0, d, jm, p, fpc = self.fit(X)
+        return self.get_labels_from_probs(u)
+
+    def predict(self, X):
+        X = self._apply_normalization_and_weights(X)
+        u, u0, d, jm, p, fpc = skfuzzy.cmeans_predict(
+            test_data=X.T,
+            cntr_trained=self.centroids,
+        )
+        return self.get_labels_from_probs(u)
+
+    def fit_partial(self):
+        raise NotImplementedError(
+            "FuzzyCMeans does not implement fit_partial. "
+            "It must be trained on the full dataset at once, not in batches. "
+            "Either increase batch size to cover the whole dataset (if your memory can take it) "
+            "or use a smaller dataset."
+        )
